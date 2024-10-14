e6a07f060500aea0353cf00995a5e002c1f2c66b
hansand02 <91554059+hansand02@users.noreply.github.com>
2024-10-14 15:42:06 +0200

    fix: refactor the way config is handled to ensure secrecy

---
+++ config_template.py
@@ -0,0 +1,5 @@
+# CHANGE NAME OF FILE TO config.py
+
+password = "Pa55worD"  # change to your password
+username = 'my@mail.invalid'  # change to your mail
+group_id = 'C9DC791FFE63D7914D6952BE10D97B46'  # change to your group id - fake
